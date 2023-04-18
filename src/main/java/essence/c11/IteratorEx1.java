11d72dbe16ae2c2eff1af4f09b508a519323b7a3
mini8022 <mini8022@tsis.co.kr>
2023-04-19 05:53:31 +0900

    자바의 정석 11장 Iterator 예제

---
+++ src/main/java/essence/c11/IteratorEx1.java
@@ -0,0 +1,22 @@
+package essence.c11;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class IteratorEx1 {
+    public static void main(String[] args){
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(30);
+
+        Iterator it = list.iterator();
+
+        while (it.hasNext()) {
+            Object obj = it.next();
+            System.out.println("obj = " + obj);
+        }
+        
+    }
+}
