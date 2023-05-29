498d948592d05e79aa1a8c40bf11ff6b05d7dce4
mini8022 <mini8022@tsis.co.kr>
2023-05-30 06:01:32 +0900

    Sort 역순정렬

--- src/main/java/essence/c11/ComparatorEx.java
+++ src/main/java/essence/c11/ComparatorEx.java
@@ -1,6 +1,7 @@
 package essence.c11;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class ComparatorEx {
 
@@ -13,7 +14,21 @@ public class ComparatorEx {
         Arrays.sort(strArr, String.CASE_INSENSITIVE_ORDER); //대소문자 구분안함
         System.out.println("Arrays.toString(strArr) = " + Arrays.toString(strArr));
 
-//        Arrays.sort(strArr, new Descending()); //대소문자 구분안함
-//        System.out.println("Arrays.toString(strArr) = " + Arrays.toString(strArr));
+        Arrays.sort(strArr, new Descending()); //역순 정렬
+        System.out.println("Arrays.toString(strArr) = " + Arrays.toString(strArr));
     }
 }
+
+    class Descending implements Comparator{
+        @Override
+        public int compare(Object o1, Object o2) {
+            if (o1 instanceof Comparator && o2 instanceof Comparator) {
+                Comparable c1 = (Comparable) o1;
+                Comparable c2 = (Comparable) o2;
+
+                return c1.compareTo(o2) * -1; // -1을 곱해서 기본 정렬방식의 역으로 변경
+                                              // 또는 c2.compareTo(o1) 같이 순서를 바꿔도 된다.
+            }
+            return -1;
+        }
+    }
